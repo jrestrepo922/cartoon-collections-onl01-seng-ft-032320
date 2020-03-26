2916618389c03f3095ccab75cf171c1218213f52
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-26 14:56:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,84 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+#Method1 
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+  array_method1 =  ["Doc", "Dopey", "Bashful", "Grumpy"]
+  
+  
+  def roll_call_dwarves(array)  
+    new_array = []
+    i = 0
+    array.each_with_index { |name, index|
+    new_array << "#{index + 1}. #{name}"
+    }
+    
+    while new_array.length > i 
+      puts new_array[i]
+      
+      i += 1
+    end
+  end
+  
+  roll_call_dwarves(array_method1)
+  
+  #1.*Dopey
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+#method2 
+
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+  def summon_captain_planet(array) 
+    array.collect { |element|
+      element.capitalize + "!"
+    }
+  end
+  
+  summon_captain_planet(planeteer_calls)
+  
+  #summon_captain_planet(planeteer_calls)
+  #=> ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  
+  
+
+
+
+#method3
+
+  short_words = ["puff", "go", "two"]
+  assorted_words = ["two", "go", "industrious", "bop"]
+
+  def long_planeteer_calls(array)
+    array.any? { |word|
+    word.length > 4
+    }
+  end
+
+  what_is_it = long_planeteer_calls(assorted_words)
+
+
+
+#method4
+
+  snacks = ["crackers", "gouda", "thyme"]
+ 
+  
+  def find_the_cheese(array)
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find { |string|
+      string == cheese_types
+    }
+  
+  end
+  
+  find_the_cheese(snacks)
+  #=> "gouda"
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
